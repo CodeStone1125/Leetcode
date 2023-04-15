51ffa5b5d054b7f3225e8f68110868e599b147b7
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-04-15 20:32:00 +0800

    Time: 2 ms (88.27%), Space: 59 MB (77.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,12 +4,15 @@ class Solution {
         int current=0;
         int l=0; int r=1;
 
-        while(r<prices.length){
+        while(r<prices.length){  //右價大於左價
             if(prices[l]<prices[r]){
                 current=prices[r]-prices[l];
                 profit=Math.max(current,profit);
             }
-            else{
+            else if(prices[l]==prices[r]){
+                
+            }
+            else{  //右價小於左價
                 l=r;
             }
             r++;
