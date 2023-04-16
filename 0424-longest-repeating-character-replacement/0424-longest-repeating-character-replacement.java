3184cafd2f0b8ac9840ae61afc6c75d1d570e096
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-04-16 21:52:12 +0800

    Time: 26 ms (26.29%), Space: 42.8 MB (23.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -2,10 +2,12 @@ class Solution {
     public int characterReplacement(String s, int k) {
         int Max=0;
         int l=0;
+        int maxf=0;
         HashMap<Character, Integer> count = new HashMap<>();
         for(int r=0;r<s.length();r++){
             count.put(s.charAt(r), count.getOrDefault(s.charAt(r), 0) + 1);
-            while(((r-l+1)-getMaxValue(count))>k)
+            maxf=Math.max(maxf,count.getOrDefault(s.charAt(r), 0));
+            while(((r-l+1)-maxf)>k)
             {
                 count.put(s.charAt(l), count.getOrDefault(s.charAt(l), 0) - 1);
                 l++;     
@@ -15,15 +17,6 @@ class Solution {
          }
 
          return Max;      
-    }
-    public static <K, V extends Comparable<V>> V getMaxValue(Map<K, V> map) {
-        V maxValue = null;
-        for (V value : map.values()) {
-            if (maxValue == null || value.compareTo(maxValue) > 0) {
-                maxValue = value;
-            }
-        }
-        return maxValue;
     }
 }
 
