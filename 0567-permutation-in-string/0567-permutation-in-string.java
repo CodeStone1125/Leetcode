c82340eb785f27208b2142d06e41668f4704e38a
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-04-18 03:13:42 +0800

    Time: 112 ms (24.26%), Space: 43.4 MB (6.80%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,11 +2,10 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         System.out.printf("s1=%d,s2=%d\n",s1.length(),s2.length());
         int l=0;
-        int r=s1.length()-1;
-        int score=0;
+        int match=0;
         int[] count1 = new int[26];
         int[] count2 = new int[26];
-        
+        int index=0;
         if(s1.length()>s2.length()){
             return false;
         }
@@ -15,33 +14,38 @@ class Solution {
             count1[s1.charAt(i)-'a']+=1;
             count2[s2.charAt(i)-'a']+=1;
         }
-        
-        // 找遍map
-        while(r<s2.length()){
-            score=0;
-            for (int i=0;i<26;i++) { //確認是substring的permutation
+        for (int i=0;i<26;i++) { //確認是substring的permutation
                 if(count1[i]==count2[i]){ //確認所有字母出現次數
-                    score+=1;   //相同則加分
+                    match+=1;   //相同則加分
                     
                 }
-                else{ //有字母次數不同
-                    break;
-                }
-                
-            }
-            System.out.printf("l=%d,r=%d\n",l,r);
-            if(score==26) //如果找到
-            {
+        }
+
+        for(int r=s1.length();r<s2.length();r++){
+            if(match==26){ //如果找到
                 return true;
             }
-            if(r==(s2.length()-1)){ //window到底了
-                break;
+            System.out.println(match);
+            index=s2.charAt(r)-'a'; //右指標
+            count2[index]+=1;
+            if(count2[index]==count1[index]){
+                match+=1;
             }
-            count2[s2.charAt(l)-'a']-=1;//window整個右移
-            r++;
+            else if(count2[index]==count1[index]+1){
+                match-=1;
+            }
+            index=s2.charAt(l)-'a'; //左指標
+            count2[index]-=1;
+            if(count2[index]==count1[index]){
+                match+=1;
+            }
+            else if(count2[index]==count1[index]-1){
+                match-=1;
+            }
             l++;
-            count2[s2.charAt(r)-'a']+=1;
-            
+        }
+        if(match==26){ //如果找到
+                return true;
         }
         return false;
     }
