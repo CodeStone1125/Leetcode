0585300202010a45a3b4ce96f9801a31ff480ceb
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-04-17 23:27:06 +0800

    Time: 2033 ms (5.02%), Space: 43.4 MB (6.80%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,52 +1,50 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()){
+        System.out.printf("s1=%d,s2=%d\n",s1.length(),s2.length());
+        int l=0;
+        int r=s1.length()-1;
+        int score=0;
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+        
+        if(s1.length()>s2.length()){
             return false;
         }
-        int p1 = 0;
-        int p2 = 0;
-
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int i = 0 ; i < s1.length(); i++){
-            if(map.containsKey(s1.charAt(i))){
-                map.put(s1.charAt(i), map.get(s1.charAt(i))+1);
-            }
-            else{
-                map.put(s1.charAt(i), 1);
-            }
+        
+        for(int i=0;i<s1.length();i++){
+            count1[s1.charAt(i)-'a']+=1;
+            count2[s2.charAt(i)-'a']+=1;
         }
-        // we now know the size of the window 
-        HashMap<Character, Integer> map2 = new HashMap<>();
-        ArrayList<Character> list = new ArrayList<>();
-        for(int i = 0 ; i < s1.length(); i++){
-            list.add(s2.charAt(i));
-            if(map2.containsKey(s2.charAt(i))){
-                map2.put(s2.charAt(i), map2.get(s2.charAt(i))+1);
+        
+        // 找遍map
+        while(r<s2.length()){
+            score=0;
+            for (int i=0;i<26;i++) { //確認是substring的permutation
+                if(count1[i]==count2[i]){ //確認所有字母出現次數
+                    score+=count1[i];   //相同則加分
+                    System.out.printf("s1的%d有%d個s2的%d有%d個，score=%d\n",i,count1[i],i,count2[i],score);
+                    
+                }
+                else{ //有字母次數不同
+                    System.out.printf("s1的%s有%d個s2的%s有%d個，\n",i,count1[i],i,count2[i]);
+                    break;
+                }
+                
             }
-            else{
-                map2.put(s2.charAt(i), 1);
-            }
-        }
-        if(map2.equals(map)){
-            return true;
-        }
-        for(int i = s1.length(); i < s2.length(); i++){
-            char ch = list.get(0);
-            list.remove(0);
-            list.add(s2.charAt(i));
-            map2.put(ch, map2.get(ch)-1);
-            if(map2.get(ch) == 0){
-                map2.remove(ch);
-            }
-            if(map2.containsKey(s2.charAt(i))){
-                map2.put(s2.charAt(i), map2.get(s2.charAt(i))+1);
-            }
-            else{
-                map2.put(s2.charAt(i), 1);
-            }
-            if(map2.equals(map)){
+            System.out.printf("l=%d,r=%d\n",l,r);
+            if(score==s1.length()) //如果找到
+            {
                 return true;
             }
+            if(r==(s2.length()-1)){ //window到底了
+                System.out.println("跳脫舒適圈");
+                break;
+            }
+            count2[s2.charAt(l)-'a']-=1;//window整個右移
+            r++;
+            l++;
+            count2[s2.charAt(r)-'a']+=1;
+            
         }
         return false;
     }
