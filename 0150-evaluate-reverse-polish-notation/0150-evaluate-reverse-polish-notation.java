85ed292b5efc75d8fd538d23f0095468d5492563
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-04-18 16:45:05 +0800

    Time: 8 ms (33.90%), Space: 42.1 MB (82.31%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -5,31 +5,26 @@ class Solution {
         for(int i=0;i<tokens.length;i++){
             if(Character.isDigit(tokens[i].charAt(tokens[i].length()-1))){
                 stack.push(Integer.parseInt(tokens[i]));
-                System.out.println("push");
             }
             else if(tokens[i].equals("+")){
                 op2=stack.pop();
                 op1=stack.pop();
                 stack.push(op1+op2);
-                System.out.printf("%d+%d\n",op1,op2);
             }
             else if(tokens[i].equals("-")){
                 op2=stack.pop();
                 op1=stack.pop();
                 stack.push(op1-op2);
-                System.out.printf("%d-%d\n",op1,op2);
             }
             else if(tokens[i].equals("*")){
                 op2=stack.pop();
                 op1=stack.pop();
                 stack.push(op1*op2);
-                System.out.printf("%d*%d\n",op1,op2);
             }
             else if(tokens[i].equals("/")){
                 op2=stack.pop();
                 op1=stack.pop();
                 stack.push(op1/op2);
-                System.out.printf("%d/%d\n",op1,op2);
             }
         }
         return stack.pop();
