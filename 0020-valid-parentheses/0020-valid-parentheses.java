d3147d843ad3c40b5ca71966596e680019468998
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-04-18 15:12:13 +0800

    Time: 3 ms (13.62%), Space: 40.4 MB (82.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,10 +1,8 @@
 import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
-        int temp=0;
         Stack<Character> stack = new Stack<>();
         for(int i=0;i<s.length();i++){
-            System.out.println(s.charAt(i));
             if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
                 stack.push(s.charAt(i));
             }
