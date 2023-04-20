7ba2d2840ddeea8b39ce90264191e21a977dd68f
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-04-20 08:57:34 +0800

    Time: 1 ms (100.00%), Space: 45.9 MB (20.35%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -13,7 +13,6 @@ class Solution {
         ListNode list1 = new ListNode();
         ListNode tail1 = list1;
         ListNode list2 = new ListNode();
-        ListNode tail2 = list2;
         ListNode ans = new ListNode();
         ListNode tail = ans;
         ListNode temp = head;
@@ -32,8 +31,8 @@ class Solution {
         list1=list1.next;
         list2=list2.next;        
         list2 = reverseList(list2);
-        printLinkedList(list1);
-        printLinkedList(list2);
+        // printLinkedList(list1);
+        // printLinkedList(list2);
         while(list1!=null){
             tail.next=list1;
             list1=list1.next;
@@ -63,11 +62,11 @@ class Solution {
         return q;
     }
     
-    void printLinkedList(ListNode head) {
-        while (head != null) {
-            System.out.print(head.val + " ");
-            head = head.next;
-        }
-        System.out.println();
-    }
+//     void printLinkedList(ListNode head) {
+//         while (head != null) {
+//             System.out.print(head.val + " ");
+//             head = head.next;
+//         }
+//         System.out.println();
+//     }
 }
