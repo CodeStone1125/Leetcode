5667da4e84dd6371f7ca27439c4f4571326f526e
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-04-21 02:50:37 +0800

    Time: 82 ms (14.23%), Space: 118.2 MB (33.17%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,77 +1,73 @@
 class LRUCache {
-
     private Map<Integer, Node> cache;
     private int capacity;
-
+    
     private Node left;
-    private Node right;
+    private Node right; 
 
+    
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        cache = new HashMap<>();
-
-        //left = LRU , right = most recent
-        this.left = new Node(0, 0);
-        this.right = new Node(0, 0);
+        cache = new HashMap<>(); 
+        
+        this.left = new Node(0,0);//left: LRU , right: recent
+        this.right = new Node(0,0);
         this.left.next = this.right;
         this.right.prev = this.left;
     }
-
+    
     public int get(int key) {
-        if (cache.containsKey(key)) {
+        if(cache.containsKey(key)){
             remove(cache.get(key));
             insert(cache.get(key));
             return cache.get(key).val;
-        } else {
+        }
+        else{
             return -1;
         }
     }
-
+    
     public void put(int key, int value) {
-        if (cache.containsKey(key)) {
+        if(cache.containsKey(key)){
             remove(cache.get(key));
         }
         cache.put(key, new Node(key, value));
         insert(cache.get(key));
-
-        if (cache.size() > capacity) {
-            // remove from the list and delte the LRU from the hashmap
-            Node lru = this.left.next;
+        
+        if(cache.size()>capacity){
+            Node lru = left.next;
             remove(lru);
             cache.remove(lru.key);
         }
     }
-
-    // remove node from list
-    public void remove(Node node) {
-        Node prev = node.prev;
-        Node next = node.next;
-
-        prev.next = next;
-        next.prev = prev;
-    }
-
-    // insert node at right
-    public void insert(Node node) {
+    
+    public void insert(Node node)  {
         Node prev = this.right.prev;
         Node next = this.right;
-
         prev.next = node;
         next.prev = node;
-
         node.next = next;
         node.prev = prev;
+        
+    }
+    
+    public void remove(Node node)  {
+        Node prev = node.prev;
+        Node next = node.next;
+        
+        prev.next = next;
+        next.prev = prev;
+        
     }
 
-    private class Node {
-
+    private class Node{
         private int key;
         private int val;
-
+        
         Node next;
         Node prev;
-
-        public Node(int key, int val) {
+        
+        public Node(int key,int val){
             this.key = key;
             this.val = val;
         }
