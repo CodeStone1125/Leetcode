b99eace6f9106b94eb93496a4dc0f1b8fffce7fb
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-04-21 22:10:20 +0800

    Time: 15 ms (6.84%), Space: 42.9 MB (40.95%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        traverse(root, 1);
+        List<List<Integer>> ans = new ArrayList<>(map.values());
+        return ans;
+        
+    }
+   public void traverse(TreeNode root, int height) {
+        if (root == null) {
+            return;
+        }
+        // Print the height of the current node
+        System.out.println("Node " + root.val + " is at height " + height);
+        List<Integer> temp = map.getOrDefault(height,new ArrayList<Integer>());
+        temp.add(root.val);
+        map.put(height,temp);
+        // Traverse the left subtree
+        traverse(root.left, height + 1);
+        // Traverse the right subtree
+        traverse(root.right, height + 1);
+    }
+}
