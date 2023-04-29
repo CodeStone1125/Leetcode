19c4cf5af59dd6c6d6031410cd65c78a79e9374f
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-04-29 21:00:02 +0800

    Time: 1 ms (64.86%), Space: 41.3 MB (55.43%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -26,22 +26,22 @@ class Solution {
     public void bfs(List<Integer> ans, TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        System.out.println("root " + root.val);
+        // System.out.println("root " + root.val);
         while(!q.isEmpty()){
             int levelSize = q.size();
             for(int i=levelSize;i>=1;i--){ //BFS
                 TreeNode cur = q.poll();
                 if(i==1) {
                     ans.add(cur.val);
-                    System.out.println("ans加入 " + cur.val);
+                    // System.out.println("ans加入 " + cur.val);
                 } //加入最右邊的元素
                 if(cur.left !=null ) {
                     q.add(cur.left); 
-                    System.out.println("加入 " + cur.left.val);
+                    // System.out.println("加入 " + cur.left.val);
                 }
                 if(cur.right !=null ) {
                     q.add(cur.right); 
-                    System.out.println("加入 " + cur.right.val);
+                    // System.out.println("加入 " + cur.right.val);
                 }
             }
             
