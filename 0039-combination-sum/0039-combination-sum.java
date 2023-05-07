3c404e704f791846add906c44d9919780a69d0c5
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-05-07 16:02:29 +0800

    Time: 315 ms (5.03%), Space: 43.2 MB (17.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
+    List<Integer> list = new ArrayList();
+    List<List<Integer>> ans = new ArrayList();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        dfs(candidates, target, 0, new ArrayList<>(), res);
-        return res;
+        helper(candidates, target, 0);
+        return ans;
     }
+    public void helper(
+        int[] candidates   
+        , int target
+        , int start
+    ){
 
-    private void dfs(int[] candidates, int target, int start, List<Integer> cur, List<List<Integer>> res) {
-        if (target == 0) {
-            res.add(new ArrayList<>(cur));
+        System.out.println(list);
+        if(target == 0){
+            ans.add(new ArrayList<>(list));
             return;
         }
-        if (target < 0) {
+        if(start > candidates.length - 1|| 0 > target){
             return;
-        }
-        for (int i = start; i < candidates.length; i++) {
-            cur.add(candidates[i]);
-            dfs(candidates, target - candidates[i], i, cur, res);
-            cur.remove(cur.size() - 1);
-        }
+        }        
+        
+        list.add(candidates[start]);
+        helper(candidates, target-candidates[start], start);
+        list.remove(list.size()-1);
+        helper(candidates, target, start + 1);
+        
     }
 }
