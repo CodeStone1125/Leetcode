b1702eda0795062bd112aa1bbf3f8fba334fc0a9
Max Chu <72511296+Max-Chu1125@users.noreply.github.com>
2023-08-11 23:26:36 +0800

    Time: 8 ms (18.31%), Space: 44 MB (70.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -11,13 +11,6 @@ class Solution {
             list.add(num[1]);
             Map.put(num[0],list);
         }
-        // // 使用 entrySet() 方法遍历 HashMap
-        // for (Map.Entry<Integer, List<Integer>> entry : Map.entrySet()) {
-        //     int key = entry.getKey();
-        //     List<Integer> values = entry.getValue();
-        //     System.out.println("Key: " + key + ", Values: " + values);
-        // }
-        
         //check all courses
         
         for(int i= 0; i< numCourses; i++){
@@ -29,23 +22,11 @@ class Solution {
     }
     public boolean check(int num, HashMap<Integer, List<Integer>> Map, HashSet<Integer> VisitSet){
         List<Integer> list = Map.getOrDefault(num, new ArrayList<Integer>());
-        if(list.size() == 0){
-            return true;
-        }
-        if(VisitSet.contains(list.get(0))){
-            // System.out.printf("%d:",num);
-            // System.out.println(Arrays.toString(list.toArray()));
-            return false;
-        } 
-        
+        if(list.size() == 0) return true;
+        if(VisitSet.contains(list.get(0))) return false;
         while(list.size()!= 0){
             int x = list.remove(0);
             VisitSet.add(x);
-            // Iterator itr = VisitSet.iterator();
-            // while (itr.hasNext()) {
-            //     System.out.printf(" %d ",itr.next());
-            // }
-            // System.out.println();
             Map.put(num,list);
             if(!check(x, Map, VisitSet)) return false;
             VisitSet.remove(x);
